4c1487fadfc3bd39ca7b3bf1139423924b46a7aa
Coqui2 <francomarcolongo98@gmail.com>
2023-08-10 15:12:39 -0300

    se agrega punto 8

--- src/Ejercicio1/Principal.java
+++ src/Ejercicio1/Principal.java
@@ -4,7 +4,23 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		
+		Empleado[] emps = new Empleado[5];
+		emps[0] = new Empleado();
+		emps[0].setNombre("Ramiro");
+		emps[0].setEdad(15);
+		emps[1] = new Empleado();
+		emps[1].setNombre("Juana");
+		emps[1].setEdad(33);
+		emps[2] = new Empleado("Jose",30);
+		emps[3] = new Empleado("Rita", 24);
+		emps[4] = new Empleado("Noa", 18);
+		
+		for (Empleado empleado : emps) {
+			System.out.println(empleado.toString());
+		}
+		
+		System.out.println(Empleado.devuelveProximoID());
 	}
 
 }
